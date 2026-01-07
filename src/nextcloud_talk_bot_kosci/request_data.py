868f1470ca6a69efaee20a0bfa131ef82a1dc815
bkosciow <kosci1@gmail.com>
2026-01-07 20:04:22 +0100

    typing

--- src/nextcloud_talk_bot_kosci/request_data.py
+++ src/nextcloud_talk_bot_kosci/request_data.py
@@ -1,27 +1,27 @@
-import json
 from typing import Dict, Any
 
+import json
 
 class RequestData:
-    def __init__(self, raw):
-        self.raw = raw
+    def __init__(self, raw: Dict[str, Any]):
+        self.raw: Dict[str, Any] = raw
 
-    def __getattr__(self, item):
+    def __getattr__(self, item: str) -> Any:
         return self._get_value(item)
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: str) -> Any:
         return self._get_value(item)
 
-    def _get_value(self, item):
+    def _get_value(self, item: str) -> Any:
         if item == 'content':
-            return json.loads(self.raw['object']['content'])
+            return json.loads(self.raw['object']['content'])  # type: Dict[str, Any]
         if item == 'message':
-            return json.loads(self.raw['object']['content'])['message']
+            return json.loads(self.raw['object']['content'])['message']  # type: str
 
         return self.raw[item] if item in self.raw else None
 
-    def __repr__(self):
-        return self.raw
+    def __repr__(self) -> str:
+        return json.dumps(self.raw)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return json.dumps(self.raw)
